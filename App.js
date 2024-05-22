b79694f1d613cad6ac237b6348ff8e901cb9d9c7
Nurul456 <you@105841101821e.student.unismuh.ac.id>
2024-05-22 14:16:38 +0800

    view

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.row}>
+
+        <View style={[styles.column, { backgroundColor: 'yellow' }]} />
+        < View style={[styles.column, { backgroundColor: 'green' }]} />
+      </View>
+      <View style={styles.row}>
+        <View style={[styles.column, { backgroundColor: 'red' }]} />
+      </View>
+      <View style={styles.row}>
+        <View style={[styles.column, { backgroundColor: 'blue' }]} />
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  column: {
+    flex: 1,
   },
 });
