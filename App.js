58cbe2035bd3c4e31ecb65f9454601d0ee859ca3
Nurul456 <you@105841101821e.student.unismuh.ac.id>
2024-05-30 00:54:59 +0800

    tugas2

--- App.js
+++ App.js
@@ -1,57 +1,68 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet,Text,View } from "react-native"
 
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{
-        flex: 1,
+    <View style={{flex :1}}>
+      <View style= {{ 
+        flex:1,
         alignItems: "flex-end",
         justifyContent: "space-between",
-        flexDirection: "row",
-        marginBottom: 30
-        
+       flexDirection: "row",
+       marginBottom: 30
+
       }}>
-        <View style={{
-          backgroundColor: "gray", width: 175, height: 60,
-          justifyContent: "center",
-          borderRadius: 15,
-          marginRight: 15,
-          marginBottom: 15,
 
-        }}>
-          <Text style={{
-            fontSize: 21, color: "black",
-            textAlign: "center",
-            fontWeight: "bold",
-          }}>
-            Login
-          </Text>
-        </View>
+    <View style= {{
+      backgroundColor: "gray",
+      width: 175,
+      height: 60,
+      margin: 15,
+      justifyContent: "center",
+      borderRadius: 15,
+      marginBottom: 15,
+      marginRight: 15,
+  
+    }}>
 
-        <View style={{
-          backgroundColor: "gray", width: 175, height: 60,
-          justifyContent: "center",
-          borderRadius: 15,
-          marginLeft: 15,
-          marginBottom: 15,
+    <Text style={{
+      fontSize: 20,
+      color: "black",
+      textAlign: "center",
+      fontWeight: "bold"
 
-        }}>
-          <Text style={{
-
-            fontSize: 21, color: "black",
-            textAlign: "center",
-            fontWeight: "bold",
-          }}>
-            Register
-          </Text>
-        </View>
-
-      </View>
+    }}>
+      Login
+    </Text>
 
     </View>
+    <View style= {{
+      backgroundColor: "gray",
+      width: 175,
+      height: 60,
+      justifyContent: "center",
+      borderRadius: 16,
+      marginBottom: 16,
+      marginRight: 16,
+  
+    }}>
+
+    <Text style={{
+      fontSize: 20,
+      color: "black",
+      textAlign: "center",
+      fontWeight: "bold"
+
+    }}>
+      Register
+    </Text> 
+ </View>
+
+</View> 
+
+</View>
 
   )
-}
+} 
 
-export default App;
+export default App
