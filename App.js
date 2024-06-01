6172ea5b0b9114835992d36f2476c6daf94fd4f9
Nurul456 <you@105841101821e.student.unismuh.ac.id>
2024-06-02 00:25:35 +0800

    Tugas ke 3

--- App.js
+++ App.js
@@ -1,68 +1,93 @@
 import React from "react";
-import { StyleSheet,Text,View } from "react-native"
+import { StyleSheet, Text, View, TextInput, Image } from "react-native";
 
 const App = () => {
   return (
-    <View style={{flex :1}}>
-      <View style= {{ 
-        flex:1,
-        alignItems: "flex-end",
-        justifyContent: "space-between",
-       flexDirection: "row",
-       marginBottom: 30
-
-      }}>
-
-    <View style= {{
-      backgroundColor: "gray",
-      width: 175,
-      height: 60,
-      margin: 15,
-      justifyContent: "center",
-      borderRadius: 15,
-      marginBottom: 15,
-      marginRight: 15,
-  
-    }}>
-
-    <Text style={{
-      fontSize: 20,
-      color: "black",
-      textAlign: "center",
-      fontWeight: "bold"
-
-    }}>
-      Login
-    </Text>
-
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Welcome Back 
+        </Text>
+      <Image
+        source={require('./assets/logo1.jpg')}
+        style={styles.image}
+      />
+      <View style={styles.inputContainer}>
+        <Text style={styles.label}>
+          Email Address
+        </Text>
+        <TextInput style={styles.input} placeholder="Enter email address" />
+        <Text style={styles.label}>
+          Password
+          </Text>
+        <TextInput style={styles.input} placeholder="Enter password" secureTextEntry />
+        <Text style={styles.forgotPassword}>
+          Forget password?
+          </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            Login
+            </Text>
+        </View>
+      </View>
     </View>
-    <View style= {{
-      backgroundColor: "gray",
-      width: 175,
-      height: 60,
-      justifyContent: "center",
-      borderRadius: 16,
-      marginBottom: 16,
-      marginRight: 16,
-  
-    }}>
+  );
+};
 
-    <Text style={{
-      fontSize: 20,
-      color: "black",
-      textAlign: "center",
-      fontWeight: "bold"
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: 210,
+    height: 210,
+    marginVertical: 30,
+  },
+  inputContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  label: {
+    marginBottom: 10,
+  },
+  input: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  forgotPassword: {
+    textAlign: 'right',
+    marginBottom: 30,
+    borderColor: 'blue',
+    color: '#0172CB',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  button: {
+    backgroundColor: '#0172CB',
+    width: 200,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'white',
+  },
+});
 
-    }}>
-      Register
-    </Text> 
- </View>
+export default App;
 
-</View> 
-
-</View>
-
-  )
-} 
-
-export default App
