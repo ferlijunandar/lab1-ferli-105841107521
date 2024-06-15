69cdb8692ef34b1860cb7051868b4a1f119e4266
Nurul456 <you@105841101821e.student.unismuh.ac.id>
2024-06-15 15:53:10 +0800

    Button component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, Image } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const App = () => {
   const ButtonCustom = ({ color, text }) => {
@@ -9,29 +9,18 @@ const App = () => {
       </View>
     );
   };
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome Back</Text>
-      <Image
-        source={require('./assets/logo1.jpg')}
-        style={styles.image}
-      />
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email Address</Text>
-        <TextInput style={styles.input} placeholder="Enter email address" />
-        <Text style={styles.label}>Password</Text>
-        <TextInput style={styles.input} placeholder="Enter password" secureTextEntry />
-        <Text style={styles.forgotPassword}>Forget password?</Text>
-      </View>
-      <View style={styles.buttonRow}>
-        <ButtonCustom color="red" text="Login" />
-        <ButtonCustom color="blue" text="Register" />
-      </View>
-    </View>
-  );
+return (
+  <View style={ styles.container}>
+    <Text style= {styles.title}>WELCOME BACK</Text>
+    <Image source={require('./assets/logo1.jpg')}
+    style={styles.Image}/>
+<View style={styles.buttonRow}>
+<ButtonCustom color="red" text="Login" />
+<ButtonCustom color="blue" text="Register" />
+</View>
+  </View> 
+);
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -48,31 +37,10 @@ const styles = StyleSheet.create({
     height: 210,
     marginVertical: 30,
   },
-  inputContainer: {
-    width: '100%',
-    paddingHorizontal: 20,
-  },
-  label: {
-    marginBottom: 10,
-  },
-  input: {
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 20,
-    borderRadius: 10,
-  },
-  forgotPassword: {
-    textAlign: 'right',
-    marginBottom: 30,
-    borderColor: 'blue',
-    color: '#0172CB',
-  },
   buttonRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '100%',
-    paddingHorizontal: 20,
+    width: '80%',
   },
   button: {
     width: '45%',
@@ -84,7 +52,6 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 20,
     color: 'white',
-    fontWeight: 'bold',
   },
 });
 
