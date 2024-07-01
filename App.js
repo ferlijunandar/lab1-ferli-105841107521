734607a92a56f81b02628651ba2a7697341a3f10
Nurul456 <you@105841101821e.student.unismuh.ac.id>
2024-07-01 19:00:31 +0800

    usefonts

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import Login from './components/login';
-import Singup from './components/singup';
-import ForgotPassword from './components/forgotPassword';
-const App = () => {
-  return (  
-    <ScrollView contentContainerStyle={styles.container}>
-        {/* <ForgotPassword/> */}
-      {/* <login /> */}
-      <Singup />
-    </ScrollView>
-  );
-};
+import {useFonts} from 'expo-font';
+import { View,Text } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    backgroundColor: '#fff',
-  },
-});
-
-export default App;
+export default function App() {
+  const [dapatFont] = useFonts({
+    'MetroBlack': require( './assets/Fonts/Metropolis-Black.otf'),
+    'MetroBold': require( './assets/Fonts/Metropolis-Bold.otf'),
+    'MetroLIght': require( './assets/Fonts/Metropolis-Light.otf'),
+    'MetroMedium': require( './assets/Fonts/Metropolis-Medium.otf'),
+    'MetroSemiBold': require( './assets/Fonts/Metropolis-SemiBold.otf'),
+  });
+  if (!dapatFont) {
+    return <Text>font tidak ditemukan...</Text>
+  }
+  return (
+    <View style={{
+      flex: 1,
+       justifyContent: 'center',
+        alignItems: 'center'
+        }}> 
+      <Text style= {{ fontFamily: 'MetroBlack' }}>font metropolis black</Text>
+      <Text style= {{ fontFamily: 'MetroBold' }}>font metropolis bold</Text>
+      <Text style= {{ fontFamily: 'MetroLIght' }}>font metropolis light</Text>
+      <Text style= {{ fontFamily: 'MetroMedium' }}>font metropolis medium</Text>
+      <Text style= {{ fontFamily: 'MetroSemiBold' }}>font metropolis semibold</Text>
+    </View>
+  )}
+    
